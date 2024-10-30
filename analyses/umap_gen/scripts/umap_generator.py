786acc4ea1e60b9c527bb3db0cb15afec672510d
fabian <fabian.mertloch@gmail.com>
2024-10-30 17:56:38 +0100

    First Steps

--- analyses/umap_gen/scripts/umap_generator.py
+++ analyses/umap_gen/scripts/umap_generator.py
@@ -8,37 +8,40 @@ import umap.umap_ as umap
 import plotly.express as px
 import matplotlib.pyplot as plt
 from dash import Dash, dcc, html
+import time
+
+start = time.time()
 
 # Test data
-data_read = pd.read_csv("/home/felixl/PycharmProjects/cellulases/data/filtered/eukaryots.phyloprofile", sep="\t")
+# data_read = pd.read_csv("/home/felixl/PycharmProjects/cellulases/data/filtered/eukaryots.phyloprofile", sep="\t")
+data_read = pd.read_csv("/home/felixl/PycharmProjects/cellulases/data/filtered/eukaryots.phyloprofile", sep="\t", nrows=20000)
+# data_read = pd.read_csv("/share/gluster/Projects/vinh/fdog_ms/pp_cbm_chitin_cellulase/cell_wall.phyloprofile", sep="\t")
 print(len(data_read))
-data_read.dropna(how='any')
+data_read.dropna(how='any', inplace=True)
 print(len(data_read))
 
-# Test UMAP
+# gene classes
+list_cl = set()
+for line in data_read.index:
+    ID = data_read["geneID"][line]
+    spliced = ID.split("_")
+    list_cl.add(spliced[0])
+print(list_cl)
+
+# UMAP
 reducer = umap.UMAP()
-umap_data = data_read[
-    [
-        "FAS_F",
-        "FAS_B"
-    ]
-].values
-scaled_data = StandardScaler().fit_transform(umap_data)
-embedding = reducer.fit_transform(scaled_data)
+features = data_read[['FAS_F']].values
+projec = reducer.fit_transform(features)
+#projec = reducer.fit_transform(features)
 
-
-fig = px.scatter(
-    x=embedding[:, 0],
-    y=embedding[:, 1],
-    #color=[sns.color_palette()[x] for x in penguins.species.map({"Adelie":0, "Chinstrap":1, "Gentoo":2})],
-    title='UMAP projection of the eukaryots dataset'
-)
-
+# Visualize using Plotly
+fig = px.scatter(projec, x=projec[:,0], y=projec[:,1], color=data_read.ncbiID, labels={'color':'ncbiID'})
 fig.show()
 
-'''app = Dash()
-app.layout = html.Div([
-    dcc.Graph(figure=fig)
-])
+end = time.time()
+print(f"Time: {round(end-start)}s")
 
-app.run_server(debug=True, use_reloader=False)'''
+app = Dash()
+app.layout = html.Div([dcc.Graph(figure=fig)])
+
+app.run_server(debug=True, use_reloader=False)
